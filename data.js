2504bb95cf8594aa94ae729dc52f6c933831c8eb
tv <nitaybensitrit123@gmail.com>
2026-01-16 20:07:52 +0200

    Update data.js

--- data.js
+++ data.js
@@ -7,18 +7,22 @@ const seriesData = [
         description: "סיפורו המרגש של ציון, ילד המנסה למצוא את מקומו בעולם ועובר לגור עם משפחה אומנת בחיפה.",
         image: "https://images.maariv.co.il/image/upload/f_auto,q_auto/586339", // פוסטר הסדרה
         episodes: [
-            { 
-                name: "פרק 1", 
-                desc: "ציון מגיע למשפחת שרוני ומתחיל להסתגל לחיים החדשים.", 
-                image: "https://img.youtube.com/vi/placeholder1/0.jpg",
-                driveId: "כאן_שים_ID_של_פרק_1" 
-            },
-            { 
-                name: "פרק 2", 
-                desc: "התמודדות עם בית הספר החדש והחברים בכיתה.", 
-                image: "https://img.youtube.com/vi/placeholder2/0.jpg",
-                driveId: "כאן_שים_ID_של_פרק_2" 
-            }
+            { name: "פרק 1", desc: "תקציר הפרק", driveId: "1-TJDR-bjqha_x-DHndKd-uvGIuQU3Lz0" },
+            { name: "פרק 2", desc: "תקציר הפרק", driveId: "10Flj4VBaJ4ANqWfy8Lt0BRTPwsrITPHZ" },
+            { name: "פרק 3", desc: "תקציר הפרק", driveId: "16Uy_zP90R2V8wI8f5PcQYer3kmfZon7M" },
+            { name: "פרק 4", desc: "תקציר הפרק", driveId: "1BT6x7j_59ptqyVyoIbBaZbzeHwrBAq0b" },
+            { name: "פרק 5", desc: "תקציר הפרק", driveId: "1DXLtQqtwL44GsS3Gr70sJDuIxjjhrQ0b" },
+            { name: "פרק 6", desc: "תקציר הפרק", driveId: "1Kmwm0nDrLFPBcndl9vwr1WNhg_Pe1vxA" },
+            { name: "פרק 7", desc: "תקציר הפרק", driveId: "1QACnT01BTBwxeQFgEtgO0HJ2jKMuN94K" },
+            { name: "פרק 8", desc: "תקציר הפרק", driveId: "1Rg-WeOCT-Daba-IzGy919JHTnl2BivEy" },
+            { name: "פרק 9", desc: "תקציר הפרק", driveId: "1SGOImupPoUKNaR89lRXhaRLWhteyLZwa" },
+            { name: "פרק 10", desc: "תקציר הפרק", driveId: "1ThXyiGhyJ6-FQkLHh_ERzePmxp1LPm8V" },
+            { name: "פרק 11", desc: "תקציר הפרק", driveId: "1g6-62xZO3gXPQniuaqzMfztolZkuBGlx" },
+            { name: "פרק 12", desc: "תקציר הפרק", driveId: "1nWL_FVXbI1gCi5_rJPfdVmstn8bTJr4p" },
+            { name: "פרק 13", desc: "תקציר הפרק", driveId: "1pCWPvXCeWlnEwjB56-qbr2OrpZZaZp5I" },
+            { name: "פרק 14", desc: "תקציר הפרק", driveId: "1tkKSN3c_axX-hXTUPIC3iHAFlO9j_rA3" },
+            { name: "פרק 15", desc: "תקציר הפרק", driveId: "1vljlNJ-49LiQgdGFS_Yt6YPptlhxt5q_" },
+            { name: "פרק 16", desc: "תקציר הפרק", driveId: "1w9AXhr23xKRme5GxiWgFUkU5qVqAbLX3" }
         ]
     }
 ];
