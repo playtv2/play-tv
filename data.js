38c54b253218bf82a2f1e1fe565b57c3ddaf388f
tv <nitaybensitrit123@gmail.com>
2026-01-16 20:28:00 +0200

    Update data.js

--- data.js
+++ data.js
@@ -2,21 +2,26 @@ const seriesData = [
     {
         id: "al-atzmi",
         title: "אל עצמי",
-        description: "סיפורו של ציון...",
-        image: "https://i.ibb.co/pWv6GZ7/al-atzmi.jpg",
+        description: "סיפורו המרגש של ציון, ילד המנסה למצוא את מקומו בעולם ועובר לגור עם משפחה אומנת בחיפה.",
+        image: "https://www.yes.co.il/media/ux0f43ln/vp004414285.jpg?quality=80",
+        category: "דרמה",
         episodes: [
-            {
-                number: "1",
-                desc: "תקציר פרק 1",
-                image: "https://i.ibb.co/pWv6GZ7/ep1.jpg",
-                driveId: "1-TJDR-bjqha_x-DHndKd-uvGIuQU3Lz0"
-            },
-            {
-                number: "2",
-                desc: "תקציר פרק 2",
-                image: "https://i.ibb.co/pWv6GZ7/ep2.jpg",
-                driveId: "10Flj4VBaJ4ANqWfy8Lt0BRTPwsrITPHZ"
-            }
+            { number: "1", desc: "תקציר פרק 1", image: "", driveId: "1-TJDR-bjqha_x-DHndKd-uvGIuQU3Lz0" },
+            { number: "2", desc: "תקציר פרק 2", image: "", driveId: "10Flj4VBaJ4ANqWfy8Lt0BRTPwsrITPHZ" },
+            { number: "3", desc: "תקציר פרק 3", image: "", driveId: "16Uy_zP90R2V8wI8f5PcQYer3kmfZon7M" },
+            { number: "4", desc: "תקציר פרק 4", image: "", driveId: "1BT6x7j_59ptqyVyoIbBaZbzeHwrBAq0b" },
+            { number: "5", desc: "תקציר פרק 5", image: "", driveId: "1DXLtQqtwL44GsS3Gr70sJDuIxjjhrQ0b" },
+            { number: "6", desc: "תקציר פרק 6", image: "", driveId: "1Kmwm0nDrLFPBcndl9vwr1WNhg_Pe1vxA" },
+            { number: "7", desc: "תקציר פרק 7", image: "", driveId: "1QACnT01BTBwxeQFgEtgO0HJ2jKMuN94K" },
+            { number: "8", desc: "תקציר פרק 8", image: "", driveId: "1Rg-WeOCT-Daba-IzGy919JHTnl2BivEy" },
+            { number: "9", desc: "תקציר פרק 9", image: "", driveId: "1SGOImupPoUKNaR89lRXhaRLWhteyLZwa" },
+            { number: "10", desc: "תקציר פרק 10", image: "", driveId: "1ThXyiGhyJ6-FQkLHh_ERzePmxp1LPm8V" },
+            { number: "11", desc: "תקציר פרק 11", image: "", driveId: "1g6-62xZO3gXPQniuaqzMfztolZkuBGlx" },
+            { number: "12", desc: "תקציר פרק 12", image: "", driveId: "1nWL_FVXbI1gCi5_rJPfdVmstn8bTJr4p" },
+            { number: "13", desc: "תקציר פרק 13", image: "", driveId: "1pCWPvXCeWlnEwjB56-qbr2OrpZZaZp5I" },
+            { number: "14", desc: "תקציר פרק 14", image: "", driveId: "1tkKSN3c_axX-hXTUPIC3iHAFlO9j_rA3" },
+            { number: "15", desc: "תקציר פרק 15", image: "", driveId: "1vljlNJ-49LiQgdGFS_Yt6YPptlhxt5q_" },
+            { number: "16", desc: "תקציר פרק 16", image: "", driveId: "1w9AXhr23xKRme5GxiWgFUkU5qVqAbLX3" }
         ]
     }
 ];
