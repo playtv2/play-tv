65ce2e225228b946a835544db056a2ee31e10e0e
tv <nitaybensitrit123@gmail.com>
2026-01-16 19:46:21 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,21 @@
+// רשימת הסדרות והסרטים שלך
+const seriesData = [
+    {
+        category: "חדשים ומומלצים",
+        title: "זגורי אימפריה - פרק 1",
+        image: "https://img.youtube.com/vi/bID_1D7KqYc/0.jpg", // תמונה זמנית
+        driveUrl: "https://drive.google.com/your-video-link-1"
+    },
+    {
+        category: "הפקות מקור",
+        title: "אבא מטפלת - פרק 1",
+        image: "https://img.youtube.com/vi/bID_1D7KqYc/1.jpg", // תמונה זמנית
+        driveUrl: "https://drive.google.com/your-video-link-2"
+    },
+    {
+        category: "סדרות דרמה",
+        title: "צייר המערות - פרק 1",
+        image: "https://img.youtube.com/vi/bID_1D7KqYc/2.jpg",
+        driveUrl: "https://drive.google.com/your-video-link-3"
+    }
+];
