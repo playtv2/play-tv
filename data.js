59879c90285138ef4967ba8e996fdaf64714ea18
tv <nitaybensitrit123@gmail.com>
2026-01-17 13:07:27 +0200

    Update data.js

--- data.js
+++ data.js
@@ -16,10 +16,10 @@ const seriesData = [
         {
         id: "al-atzmi",
         type: "series", 
-        category: "סדרות", 
+        category: "ילדים", 
         title: "אל עצמי",
         description: "סיפורו המרגש של ציון.",
-        image: "https://images.maariv.co.il/image/upload/f_auto,q_auto/586339",
+        image: "https://www.yes.co.il/media/ux0f43ln/vp004414285.jpg?quality=80",
         episodes: [
             { number: "1", desc: "פרק ראשון", image: "", driveId: "1-TJDR-bjqha_x-DHndKd-uvGIuQU3Lz0" },
             { number: "2", desc: "פרק שני", image: "", driveId: "10Flj4VBaJ4ANqWfy8Lt0BRTPwsrITPHZ" }
